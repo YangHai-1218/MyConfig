a60eb599ff6caeadc8904fec7370a897833379f1
Yanghai <Yanghai1218@outlook.com>
2023-04-08 17:35:58 +0800

    add install_omz

--- convert_json_to_dat.py
+++ convert_json_to_dat.py
@@ -1,15 +1,30 @@
+#! /usr/local/bin/python3
 import json 
 import numpy as np
-with open('/Users/haiyang/Desktop/selfsup_figures/debug_loss.json', 'r') as f:
-    data = json.load(f)
+import argparse
 
-iters, loss = [], []
-for d in data:
-    iters.append(d[1]/1000)
-    loss.append(d[2]-1)
 
-iters = np.array(iters).reshape(-1)
-print(iters)
-loss = np.array(loss).reshape(-1)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('source_path', type=str)
+    parser.add_argument('target_path', type=str)
+    args = parser.parse_args()
+    return args
 
-np.savetxt('/Users/haiyang/Desktop/selfsup_figures/debug_loss.dat', np.stack([iters, loss], axis=-1), fmt=['%.2f','%.5f'])
+if __name__ == '__main__':
+    args = parse_args()
+    source_path = args.source_path
+    target_path = args.target_path
+
+    with open(source_path, 'r') as f:
+        data = json.load(f)
+
+    iters, loss = [], []
+    for d in data:
+        iters.append(d[1]/1000)
+        loss.append(d[2]-1)
+
+    iters = np.array(iters).reshape(-1)
+    loss = np.array(loss).reshape(-1)
+
+    np.savetxt(target_path, np.stack([iters, loss], axis=-1), fmt=['%.2f','%.5f'])
